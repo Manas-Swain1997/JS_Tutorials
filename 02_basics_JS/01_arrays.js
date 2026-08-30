67275ddb5fc09aaca01b7f769bc281b1bbb03f24
Manas Swain <mswain896@gmail.com>
2026-08-30 03:42:03 +0000

    arrays

--- 02_basics_JS/01_arrays.js
+++ 02_basics_JS/01_arrays.js
@@ -6,3 +6,47 @@ const myArr2 = ["Manas", "Soumya", "Bhubaneswar", "Cuttuck", "Banglore"]
 console.log(myArr[2]);
 console.log(myArr2[0]);
 
+// Array methods
+
+myArr.push(6);
+myArr.push("Manas");
+console.log(myArr);
+
+myArr.pop();
+console.log(myArr);
+
+myArr2.unshift(9);
+console.log(myArr2);
+
+myArr2.shift();
+console.log(myArr2);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(5));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+
+console.log('B', myArr);
+console.log(myArr.splice(1, 3, 22));
+console.log('c', myArr);
+// console.log(myArr.splice(0, 1, 44));
+
+
+
+
+
+
+
+
+
+
+
